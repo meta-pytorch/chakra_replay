ae4ee08d4076b9b6328b644e9f14faa6424ebe4c
shengfu-nv <shengf@nvidia.com>
2026-02-19 23:21:41 +0200

    reformat

--- et_replay/comm/backend/base_backend.py
+++ et_replay/comm/backend/base_backend.py
@@ -197,11 +197,12 @@ class BaseBackend(ABC):
     ) -> None:
         """Print startup information of the backend."""
 
+    # Collectives, if you would like more detailed documentation about the behavior of these collectives, 
+    # visit https://pytorch.org/docs/stable/_modules/torch/distributed/distributed_c10d.html.
     @abstractmethod
     def allgather_into_tensor_coalesced(self, collectiveArgs: collectiveArgsHolder, retFlag: bool = False):
         pass
 
-    # Collectives, if you would like more detailed documentation about the behavior of these collectives, visit https://pytorch.org/docs/stable/_modules/torch/distributed/distributed_c10d.html.
     @abstractmethod
     def all_reduce(self, collectiveArgs: collectiveArgsHolder, retFlag: bool = False):
         pass
